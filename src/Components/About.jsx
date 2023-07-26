7864f41a1942fb10de9e0cd780f1fa67aa68a08e
Saurabh <128633196+sir-saurabh@users.noreply.github.com>
2023-07-26 10:43:48 +0530

    Update About.jsx
    Updated details

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a UI/UX student studying at Bakhtiyarpur College of Engineering. I enjoy creating unique and simplistic user interfaces in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -37,6 +37,9 @@ const skillsList = [
   "Focus group testing",
   "Mobile user interfaces",
   "Graphic design",
+  "GitHub",
+  "Azure Platform",
+  "Machine Learning",
 ];
 
 /**
